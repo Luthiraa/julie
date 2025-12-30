0a68a0335026136bbafc375fae7854916df7698e
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2025-12-29 19:50:26 -0500

    added some new agentic features

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,6 +1,11 @@
 import "node:fs";
 import "node:os";
 import "electron";
+import "node:child_process";
 import "node:path";
 import "node:url";
+<<<<<<< HEAD
 import "./main-D1qx1LB3.js";
+=======
+import "./main-C9qdRmN6.js";
+>>>>>>> c1128b1 (messing around with agentic features)
